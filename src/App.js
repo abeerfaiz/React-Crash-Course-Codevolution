1e40021559974d5ccbc5c6443267b9654d20faa4
abeerfaiz <44645604+abeerfaiz@users.noreply.github.com>
2025-08-01 21:18:01 +0100

    learning state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet';
+import { Message } from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Bruce' heroName='Batman'/>
+      {/*<Greet name='Bruce' heroName='Batman'/>
       <Greet name='Clark' heroName='Batman'>
         <button>Action</button>  
       </Greet>
       <Greet name='Dianna' heroName='Batman'>
         <p>This is children props</p>
-      </Greet>
+      </Greet>*/}
+      <Message></Message>
     </div>
   );
 }
